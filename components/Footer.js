21583bdad87349f0dd7242c8a05670e4de14a231
Aastha Hurkat <59266988+aasthahurkat@users.noreply.github.com>
2025-05-30 14:31:41 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,18 +5,30 @@ export default function Footer() {
       role="contentinfo"
       className="
         bg-primary 
-        bg-[url('/images/mandala-pattern.png')] 
         bg-cover 
         bg-center 
-        py-6 sm:py-8 
+        py-4 sm:py-6 
         text-center
       "
     >
       <div className="container font-body text-cream text-sm sm:text-base">
         <p>
-Questions?  <a href="mailto:aasthahurkat@gmail.com" className="underline">Get in&nbsp;touch </a> 
-</p> <p>
-          Aastha&nbsp;&amp;&nbsp;Preetesh’s Wedding &mdash; December&nbsp;23&ndash;24,&nbsp;2025</p>
+          Questions?{' '}
+          <a href="mailto:aasthahurkat@gmail.com" className="underline">
+            Get in&nbsp;touch
+          </a>
+        </p>
+        <p className="mt-2">
+          <span className="block sm:inline">
+            Aastha&nbsp;&amp;&nbsp;Preetesh’s Wedding
+          </span>
+          <span className="hidden sm:inline sm:mx-1">
+            &mdash;
+          </span>
+          <span className="block sm:inline">
+            December&nbsp;23&ndash;24,&nbsp;2025
+          </span>
+        </p>
       </div>
     </footer>
   );
